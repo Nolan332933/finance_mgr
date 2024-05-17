97a68a383ef97833b1ec85ef1e80d4ec342dec15
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-05-17 22:38:55 +0700

    Redefined routes files

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,26 +1,14 @@
-import z from "zod";
-import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import authors from "./authors";
+import books from "./books";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", clerkMiddleware(), (c) => {
-  const auth = getAuth(c);
-
-  if (!auth?.userId) {
-    return c.json({
-      error: "Unauthorized",
-    });
-  }
-  return c.json({
-    message: "Hello Next.js!",
-    userId: auth.userId,
-  });
-});
+app.route("/authors", authors);
+app.route("/books", books);
 
 export const GET = handle(app);
 export const POST = handle(app);
