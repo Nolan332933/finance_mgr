6e6bb379a729719de7750caa2365e8691e52d129
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-05-18 17:21:35 +0700

    first create header section

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,5 +2,5 @@ import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <UserButton afterSignOutUrl="/" />;
+  return <div>Dashboard Page</div>;
 }
