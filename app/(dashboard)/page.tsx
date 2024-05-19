f787fb2431ab87848b6119cebb9aee9157482957
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-05-20 02:57:47 +0700

    Create Account Get API

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,22 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+// import { Button } from "@/components/ui/button";
+// import { useGetAccounts } from "@/features/accounts/api/use-get-account";
+// import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div>Dashboard Page</div>;
+  // const { data: accounts, isLoading } = useGetAccounts();
+
+  // if (isLoading) {
+  //   return <div>Loading...</div>;
+  // }
+
+  return (
+    <div>
+      {/* {accounts?.map((account) => (
+        <div key={account.id}>{account.name}</div>
+      ))} */}
+      HomePage
+    </div>
+  );
 }
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,13 +1,28 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import accounts from "./accounts";
+import { HTTPException } from "hono/http-exception";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
+  }
+  return c.json({ error: "Internal Error" }, 500);
+});
+
+const routes = app.route("/accounts", accounts);
+
 app.get("/hello", (c) => {
   return c.json({ hello: "World" });
 });
 
+// app.route("/accounts", accounts);
+
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
