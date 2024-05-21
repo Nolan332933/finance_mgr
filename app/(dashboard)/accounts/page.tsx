e5c8ccfb87cd2f7596dcea2da68764625fd38a90
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-05-21 17:51:45 +0700

    Create Data Table Components

---
+++ app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { Payment, columns } from "./columns";
+import { DataTable } from "@/components/data-table";
+
+const data: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728ed52f",
+    amount: 50,
+    status: "success",
+    email: "a@example.com",
+  },
+  // ...
+];
+
+const AccountPage = () => {
+  const newAccount = useNewAccount();
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">Account Page</CardTitle>
+          <Button onClick={newAccount.onOpen} size="sm">
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            filterKey="email"
+            columns={columns}
+            data={data}
+            onDelete={() => {}}
+            disabled
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+export default AccountPage;
