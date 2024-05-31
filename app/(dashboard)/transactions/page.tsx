9ddd93fed244468c32a0765a81d0a5f75446c6c8
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-05-31 10:32:18 +0700

    fix search function of transaction page

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -50,7 +50,7 @@ const TransactionsPage = () => {
         </CardHeader>
         <CardContent>
           <DataTable
-            filterKey="name"
+            filterKey="payee"
             columns={columns}
             data={transactions}
             onDelete={(row) => {
